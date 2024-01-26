0c403c1f492cf17929b29e15efc17c48b51ad7c1
76500833 <145300587+76500833@users.noreply.github.com>
2024-01-26 11:55:10 -0500

    styles applied to notes

--- Develop/public/assets/js/server.js
+++ Develop/public/assets/js/server.js
@@ -22,11 +22,46 @@
 //       'Content-Type': 'application/json'
 //     }
 //   });
+const express = require('express');
+const path = require('path');
+
+
+const PORT = 3001;
+
+const app = express();
+
+// Middleware for parsing JSON and urlencoded form data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Middleware to serve up static assets from the public folder
+app.use(express.static(path.join(__dirname, '../../../public')));
+
 //TODO Make route handlers for hese requests. 
-app.get('/api/notes', (req, res) => {
-    res.json({
-      term: 'api',
-      description:
-        'An application programming interface, is a computing interface that defines interactions between multiple software intermediaries',
-    });
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../../../public/index.html'));
   });
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '../../../public/notes.html'));
+  });
+
+//   app.post('/api/notes', (req, res) => {
+//     res.json({
+//       term: 'api',
+//       description:
+//         'An application programming interface, is a computing interface that defines interactions between multiple software intermediaries',
+//     });
+//   });
+
+//   app.delete('/api/notes', (req, res) => {
+//     res.json({
+//       term: 'api',
+//       description:
+//         'An application programming interface, is a computing interface that defines interactions between multiple software intermediaries',
+//     });
+//   });
+  app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
